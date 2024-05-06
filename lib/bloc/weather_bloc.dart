569c4b72f0702200da056e93ad282fa5b26b819e
Trinh Thien Bao <trinhthienbao@Trinhs-MacBook-Pro.local>
2024-05-06 21:48:25 +0700

    todayistoday

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
 
@@ -11,8 +10,7 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
     on<WeatherEvent>((event, emit) async {
       try {
-        WeatherFactory wf = WeatherFactory("7720c7654c4c815bb47a1dd6dc4e5efb",
-            language: Language.VIETNAMESE);
+        WeatherFactory wf = WeatherFactory(API, language: Language.VIETNAMESE);
 
         Position position = await Geolocator.getCurrentPosition();
 
@@ -27,3 +25,6 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
     });
   }
 }
+
+// Change your API
+final String API = "7720c7654c4c815bb47a1dd6dc4e5efb";
